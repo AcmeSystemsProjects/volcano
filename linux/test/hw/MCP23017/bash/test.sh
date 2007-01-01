bd1a98339ca13cd740597273ef881a83610b54c8
root <root@sumo.(none)>
2007-01-01 01:21:53 +0100

    update MCP23017 test file

--- linux/test/hw/MCP23017/bash/test.sh
+++ linux/test/hw/MCP23017/bash/test.sh
@@ -5,42 +5,36 @@ echo "Test for MCP23017"
 
 #MCP23017 at reset:
   #pin ALL INPUT
-#READ GPIO
-echo "buttons on GPIOB:"
-GPIO=$(i2cget -y 0 0x27 0x13 2>&1)
-echo $GPIO
+#READ GPIOA
+echo "GPIOA value:"
+GPIOA=$(i2cget -y 0 0x27 0x12 2>&1)
+echo $GPIOA
 
-#config gpioa in output mode
-i2cset -y 0 0x27 0x00 0x00
-#switch on backlight
-echo "retroilluminazione accesa"
-i2cset -y 0 0x27 0x12 0x02
+echo "Rotary selector on GPIOB:"
+GPIOB=$(i2cget -y 0 0x27 0x13 2>&1)
+SELECTOR=$(( 15 - ( GPIOB & 0x0F ) ))
+echo $SELECTOR
 
-echo "attesa 1 secondo"
+
+#config pins with led on gpiob in output mode
+i2cset -y 0 0x27 0x01 0x0F
+echo "Only D1 on"
+i2cset -y 0 0x27 0x13 0x10
+sleep 1
+echo "Only D2 on"
+i2cset -y 0 0x27 0x13 0x20
+sleep 1
+echo "Only D3 on"
+i2cset -y 0 0x27 0x13 0x40
+sleep 1
+echo "Only D4 on"
+i2cset -y 0 0x27 0x13 0x80
 sleep 1
 
-echo "retroilluminazione spenta"
-i2cset -y 0 0x27 0x12 0x00
+echo "All led off"
+i2cset -y 0 0x27 0x13 0x00
 
-echo "accendo un led per volta"
-i2cset -y 0 0x27 0x12 0x01
-sleep 0.5
-i2cset -y 0 0x27 0x12 0x04
-sleep 0.5
-i2cset -y 0 0x27 0x12 0x08
-sleep 0.5
-i2cset -y 0 0x27 0x12 0x10
-sleep 0.5
-i2cset -y 0 0x27 0x12 0x20
-sleep 0.5
-i2cset -y 0 0x27 0x12 0x40
-sleep 0.5
-i2cset -y 0 0x27 0x12 0x80
-sleep 0.5
-i2cset -y 0 0x27 0x12 0x00
-sleep 0.5
-echo "fine"
-
+echo "End"
 echo
 
 

b77a90342625ed11cfc936870f61a2c6e9c42cb6
Andrea Montefusco - IW0HDV <andrew@montefusco.com>
2016-05-15 21:46:12 +0200

    More error checking and termination using the Arietta button

--- linux/test/sw/interrupt/pulse_buzzer/pulse_buzzer.c
+++ linux/test/sw/interrupt/pulse_buzzer/pulse_buzzer.c
@@ -9,36 +9,41 @@
 #include <signal.h>
 #include <pthread.h>
 #include <linux/input.h>
- 
+
 #define LED_BRIGHTNESS    "/sys/class/leds/motherboard_buzzer/brightness"
 #define    LED_TRIGGER    "/sys/class/leds/motherboard_buzzer/trigger"
 #define    INPUT_EVENT    "/dev/input/event0"
 #define    LED_MAX_SPEED    10
 #define    PERIOD_COEFF    16000
- 
+
 unsigned int led_speed;
 pthread_mutex_t lock;
- 
+
+volatile int run = 0;
+
 /* Blink LED */
 static void *LEDMod(void *dummy)
 {
     unsigned int led_period;
     int tmp;
     tmp = open(LED_BRIGHTNESS, O_WRONLY);
-    if (tmp < 0)
+    if (tmp < 0) {
+        perror(LED_BRIGHTNESS);
         exit(1);
-    while (1) {
+    }
+    while (run) {
         pthread_mutex_lock(&lock);
         led_period = (LED_MAX_SPEED - led_speed) * PERIOD_COEFF;
         pthread_mutex_unlock(&lock);
- 
+
         write(tmp, "1", 2);
         usleep(led_period);
         write(tmp, "0", 2);
         usleep(led_period);
     }
+    write(tmp, "0", 2);
 }
- 
+
 int main()
 {
     pthread_t pth;
@@ -46,35 +51,42 @@ int main()
     int tmp;
     int key_code;
     int size = sizeof(ev);
- 
+
     /* Configure LED */
     led_speed = 5;
     tmp = open(LED_TRIGGER, O_WRONLY);
-    if (tmp < 0)
+    if (tmp < 0) {
+        perror(LED_TRIGGER);
         return 1;
+    }
 /*    if (write(tmp, "default-on", 10) != 10) {
         printf("Error writing trigger");
         return 1;
     }*/
     close(tmp);
     printf("Configured LED for use\n");
- 
-    /* Create thread */
+
     pthread_mutex_init(&lock, NULL);
-    pthread_create(&pth, NULL, LEDMod, "Blinking LED...");
- 
+    /* Create thread */
+    if (pthread_create(&pth, NULL, LEDMod, "Blinking LED...")) {
+       perror ("Creating thread");
+       return 1;
+    } else {
+       run = 1;
+    }
+
     /* Read event0 */
     tmp = open(INPUT_EVENT, O_RDONLY);
     if (tmp < 0) {
-        printf("\nOpen " INPUT_EVENT " failed!\n");
+        perror(INPUT_EVENT);
         return 1;
     }
     /* Read and parse event, update global variable */
     while (1) {
 
 	// wait for event, stand-by
-        if (read(tmp, &ev, size) < size) { 
-            printf("\nReading from " INPUT_EVENT " failed!\n");
+        if (read(tmp, &ev, size) < size) {
+            perror(INPUT_EVENT);
             return 1;
         }
 
@@ -97,9 +109,17 @@ int main()
                 if (led_speed < 9)
                     led_speed += 1;
                 pthread_mutex_unlock(&lock);
+            } else if (key_code == 28) {
+                printf(", Arietta button\n\r");
+                break;
             }
             printf("Speed: %i\n\r", led_speed);
             usleep(1000);
         }
     }
+    if (run) {
+       run = 0 ;
+       pthread_join (pth, 0);
+    }
+    return 0;
 }
